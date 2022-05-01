4fa1f69aafde2fbf0f851e358b9c8a91270cdfbe
dfchang149 <dfchang@ucsd.edu>
2022-05-01 12:09:27 -0700

    Added Failed Test

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void multiplication() {
+        assertEquals(2, SkillDemo1.multiply(1, 1)); // This test fails
+    }
+}
